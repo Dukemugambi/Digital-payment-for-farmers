ec041cc1a24a531d8c3ac3d9d9ffc1ba32868202
Adam Sommer <asommer12@gmail.com>
2013-07-31 16:24:19 -0700

    refactor for wefarm tutorial

--- app/controllers/Session.java
+++ app/controllers/Session.java
@@ -11,55 +11,53 @@ import models.*;
 
 public class Session extends Controller {
 
-    public static class Login {
+	public static class Login {
         
-        public String useremail;
-        public String password;
+		public String useremail;
+		public String password;
         
-        public String validate() {
-            if(Farmer.authenticate(useremail, password) == null) {
-                return "Invalid user or password";
-            }
-            return null;
-        }
-    }
+		public String validate() {
+			if(Farmer.authenticate(useremail, password) == null) return "Invalid user or password";
+			else return null;
+		}
+	}
     
-    public static String encryptPassword(String cleanPassword) {
-    	if (cleanPassword == null) return null;
-		return BCrypt.hashpw(cleanPassword, BCrypt.gensalt());
-    }
+	public static String encryptPassword(String cleanPassword) {
+		if (cleanPassword == null) return null;
+		else return BCrypt.hashpw(cleanPassword, BCrypt.gensalt());
+	}
+	
+	public static boolean checkPassword(String candidate, String encrypted) {
+		if (candidate == null || encrypted == null) return false;
+		else return BCrypt.checkpw(candidate, encrypted);
+	}
     
-    public static boolean checkPassword(String candidate, String encrypted) {
-    	if (candidate == null || encrypted == null) return false;
-    	return BCrypt.checkpw(candidate, encrypted);
-    }
-    
-    public static Result login() {
-        return ok(views.html.login.render(flash("message"), form(Login.class)));
-    }
+	public static Result login() {
+		return ok(views.html.login.render(flash("message"), form(Login.class)));
+	}
 	
-    public static Result authenticate() {
-        Form<Login> loginForm = form(Login.class).bindFromRequest();
-        if(loginForm.hasErrors()) {
-            flash("message", "Incorrect email or password.");
-            return redirect(routes.Session.login());
-        }
-        else {
-            session("connected", loginForm.get().useremail);
-            flash("message", "Welcome back to WeFarm.");
-            return redirect(routes.Application.index());
-        }
-    }
+	public static Result authenticate() {
+		Form<Login> loginForm = form(Login.class).bindFromRequest();
+		if(loginForm.hasErrors()) {
+			flash("message", "Incorrect email or password.");
+			return redirect(routes.Session.login());
+		}
+		else {
+			session("connected", loginForm.get().useremail);
+			flash("message", "Welcome back to WeFarm.");
+			return redirect(routes.Application.index());
+		}
+	}
 	
-    public static Result logout() {
-        session().clear();
-        flash("message", "You've been logged out");
-        return redirect(routes.Application.index());  
-    }
+	public static Result logout() {
+		session().clear();
+		flash("message", "You've been logged out");
+		return redirect(routes.Application.index());  
+	}
 	
-    public static boolean isSessionOwner(Farmer farmer) {
-        if (farmer.useremail.equals(session("connected"))) return true;
-        else return false;
-    }
+	public static boolean isSessionOwner(Farmer farmer) {
+		if (farmer.useremail.equals(session("connected"))) return true;
+		else return false;
+	}
 	
 }
