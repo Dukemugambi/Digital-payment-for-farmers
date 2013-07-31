749f37491a9978fc921e496d5d1734be857c0b70
Adam Sommer <asommer12@gmail.com>
2013-07-31 14:56:09 -0700

    added wepay initialization to startup

--- app/models/MyServerConfigStartup.java
+++ app/models/MyServerConfigStartup.java
@@ -9,13 +9,13 @@ import com.wepay.WePay;
 import controllers.Application;
 
 public class MyServerConfigStartup implements ServerConfigStartup {
-    @Override
-    public void onStart(ServerConfig serverConfig) {
+	@Override
+	public void onStart(ServerConfig serverConfig) {
 		Long appClientId = ConfigFactory.load().getLong("APP_CLIENT_ID");
 		String appClientSecret = ConfigFactory.load().getString("APP_CLIENT_SECRET");
 		Boolean useStageEnv = ConfigFactory.load().getBoolean("USE_STAGE_ENV");
 		initializeWePayObject(appClientId, appClientSecret, useStageEnv);
-        serverConfig.setDatabaseSequenceBatchSize(1);
+		serverConfig.setDatabaseSequenceBatchSize(1);
     }
     
 	public static void initializeWePayObject(Long appClientId, String appClientSecret, boolean useStageEnv) {
